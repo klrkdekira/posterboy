f8988fb74194cd4fdefaa38dde5d98d2427a620e
Chee Leong <klrkdekira@gmail.com>
2015-05-26 23:12:01 +0800

    Need better recursion. Thinking of abusing channels.

--- main.go
+++ main.go
@@ -3,31 +3,70 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
+	"runtime"
 	"strings"
+	"sync"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-const endpoint = "http://www.pos.com.my/postal-services/quick-access"
+type (
+	Address struct {
+		No         string `json:"no"`
+		Location   string `json:"location"`
+		Postcode   string `json:"postcode"`
+		PostOffice string `json:"postOffice"`
+		State      string `json:"state"`
+	}
+
+	Jobs struct {
+		WorkList  map[string]bool
+		Addresses []*Address
+		*sync.Mutex
+	}
+)
+
+const host = "http://www.pos.com.my"
+const endpoint = host + "/postal-services/quick-access/"
 
 func main() {
+	t := runtime.NumCPU()
+	runtime.GOMAXPROCS(t)
+
 	states, err := getStates()
 	if err != nil {
 		fmt.Println("failed to get state options")
 		os.Exit(1)
 	}
 
-	statesChan := make(chan string)
-	for _, s := range states {
-		statesChan <- s
-	}
+	<-processState(states[0])
+	// var wg sync.WaitGroup
+	// statesChan := make(chan string, len(states))
+	// for _, s := range states {
+	// 	wg.Add(1)
+	// 	statesChan <- s
+	// }
 
-	for _ = range states {
-		fmt.Println(<-statesChan)
-	}
-
-	// fmt.Print(states)
+	// for i := 0; i < t; i++ {
+	// 	go func(statesChan chan string) {
+	// 		for {
+	// 			select {
+	// 			case state := <-statesChan:
+	// 				success := <-processState(state)
+	// 				if !success {
+	// 					fmt.Printf("failed while processing %s, retrying in 5 seconds...\n", state)
+	// 					statesChan <- state
+	// 					<-time.After(5 * time.Second)
+	// 				} else {
+	// 					wg.Done()
+	// 				}
+	// 			}
+	// 		}
+	// 	}(statesChan)
+	// }
+	// wg.Wait()
 }
 
 func getStates() ([]string, error) {
@@ -51,6 +90,81 @@ func getStates() ([]string, error) {
 	}
 
 	return states, nil
+}
+
+func processState(state string) <-chan bool {
+	resp := make(chan bool, 1)
+	fmt.Printf("processing... %s\n", state)
+
+	jobs := &Jobs{
+		WorkList: make(map[string]bool),
+		Address:  make([]*Address, 0),
+		Mutex:    &sync.Mutex{},
+	}
+
+	params := url.Values{}
+	params.Set("postcodeFinderState", state)
+	params.Set("postcodeFinderLocation", "")
+	params.Set("page", "1")
+	url := fmt.Sprintf("%s?%s", endpoint, params.Encode())
+	jobs.WorkList[url] = false
+
+	if err := processStatePage(url, jobs); err != nil {
+		for u, ok := range jobs.WorkList {
+			if !ok {
+				if err := processState(url, jobs); err == nil {
+					break
+				}
+			}
+		}
+	} else {
+		resp <- true
+	}
+
+	return resp
+}
+
+func processStatePage(url string, jobs *Jobs) error {
+	doc, err := download(url)
+	if err != nil {
+		return err
+	}
+
+	table := doc.Find("div#postcode-finder-output table")
+	addresses := make([]*Address, 0)
+	table.Find("tbody tr").Each(func(i int, s *goquery.Selection) {
+		children := s.Children()
+		address := &Address{
+			No:         children.Eq(0).Text(),
+			Location:   children.Eq(1).Text(),
+			Postcode:   children.Eq(2).Text(),
+			PostOffice: children.Eq(3).Text(),
+			State:      children.Eq(4).Text(),
+		}
+		addresses = append(addresses, address)
+	})
+
+	jobs.Addresses = append(jobs.Addresses, addresses...)
+
+	links := table.Find("tfoot a")
+	for i := range links.Nodes {
+		s := links.Eq(i)
+		href, _ := s.Attr("href")
+		url := host + href
+		if done, found := jobs.WorkList[url]; !found {
+			jobs.WorkList[url] = false
+		} else {
+			if done {
+				// skipping this hell
+				return
+			}
+		}
+		if err := processState(url, jobs); err != nil {
+			return err
+		}
+	}
+	jobs.WorkList[url] = true
+	return nil
 }
 
 func download(url string) (*goquery.Document, error) {
